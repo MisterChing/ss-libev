930127063522a971023bf050be24df210a219cc3
misterching <chfnaruto@gmail.com>
2020-11-29 00:25:17 -0500

    delete file

--- bin/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-HOME=$(readlink -f $(dirname $0))
-WORK_DIR=`dirname $0`
-sleep 1
-if [ "$1"x == "server"x ]; then
-    ${HOME}/ss-server -c ${HOME}/../conf/config.json -f ${HOME}/../var/ss-server.pid
-elif [ "$1"x == "client"x ]; then
-    ${HOME}/ss-local -c ${HOME}/../conf/config.json -f ${HOME}/../var/ss-local.pid
-fi
